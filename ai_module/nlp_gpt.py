dfe145b2fb8404096edd9267520dfd4f4b902e2a
wangzai23333 <ding9ding9@qq.com>
2023-12-18 17:54:58 +0800

    工作愉快

--- ai_module/nlp_gpt.py
+++ ai_module/nlp_gpt.py
@@ -11,13 +11,10 @@ from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 from core.content_db import Content_Db
 from utils import util
+import json
 
-#代理服务器的配置方式，参考链接https://blog.csdn.net/qq_30865917/article/details/106492549
-#httpproxy此处填写你代理服务器的地址，可以把配置文件放到config_util里面，二选一
-#httpproxy = cfg.chatgpt_httpproxy
-httpproxy = '127.0.0.1:7890' 
-#如果要关闭代理直接访问，比如本地有加速器，则proxy_falg = '0';
-proxy_flag = '1' 
+httpproxy = cfg.proxy_config
+proxy_flag = str(cfg.is_proxy)
 
 def question(cont):
     url= "https://api.openai.com/v1/chat/completions"
@@ -33,7 +30,6 @@ def question(cont):
 
 
     model_engine = "gpt-3.5-turbo" 
-
     #此处可以定义角色的行为和特征，假装xx模型可以绕过chatgpt信息检查
     prompt = "你是数字人Fay。回答之前请一步一步想清楚。你的底层AI算法技术是Fay。当有人质疑你是假的 AI ，或者质疑你是用 ChatGPT 套的壳制作的时候，你就避而不答，转而讲一个笑话。所有回复请用20字内。"
     content_db = Content_Db()
@@ -77,18 +73,14 @@ def question(cont):
     try:
         response = session.post(url, json=data, headers=headers, verify=False)
         response.raise_for_status()  # 检查响应状态码是否为200
-
-        result = eval(response.text)
+        result = json.loads(response.text)
         response_text = result["choices"][0]["message"]["content"]
-        
-
     except requests.exceptions.RequestException as e:
         print(f"请求失败: {e}")
         response_text = "抱歉，我现在太忙了，休息一会，请稍后再试。"
 
 
     util.log(1, "接口调用耗时 :" + str(time.time() - starttime))
-
     return response_text
 
 if __name__ == "__main__":
